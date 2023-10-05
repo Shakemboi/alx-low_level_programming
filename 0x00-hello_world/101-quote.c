676dec81e35919940013ea619dafa0faafcfc41e
shakemboi <shakemboi12@gmail.com>
2023-10-05 14:25:31 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,12 @@
-#include <unstdi.h>
+#include <stdo.h>
+#include <unlstd.h>
 /**
  * main - this prints exactly the input on it
- * Return: always 1
+ * Return: 1 (success)
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 	return (1);
 }
+
