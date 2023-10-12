d4b8160c75eaca78890885be2c62e3947788e187
shakemboi <shakemboi12@gmail.com>
2023-10-12 13:22:44 -0700

    is upper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _isupper - is an uppercase
+ * @c: char to check
+ * Return: 1 or 0
+ */
+int _isupper(int c)
+{
+	if (c == 'A' && c == 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
---
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -0,0 +1,12 @@
+#include <unistd.h>
+
+/**
+ * _putchar - writes character c to stdout
+ * @c: The character to be written
+ * Return: On succes 1.
+ * On error, returns -1 and sets errno appropriately.
+ */
+int _putchar(char c)
+{
+	return (write(1, &1, 1));
+}
