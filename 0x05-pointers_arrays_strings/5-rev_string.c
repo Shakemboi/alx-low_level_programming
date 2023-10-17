b26087215031705eefa13b7be9512e05bc7dfa2d
shakemboi <shakemboi12@gmail.com>
2023-10-16 17:57:18 -0700

    reverses

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * rev_string - points a string in reverse
+ *@s: pointer pointing to the string
+ * Return: n
+ */
+void rev_string(char *s)
+{
+	int length, z, half;
+	char temp;
+
+	for (length = 0; s[length] != '\0'; length++)
+	;
+	z = 0;
+	half = length / 2;
+
+	while (half--)
+	{
+		temp = s[length - z - 1];
+		s[length - z - 1] = s[z];
+		s[z] = temp;
+	}
+}
