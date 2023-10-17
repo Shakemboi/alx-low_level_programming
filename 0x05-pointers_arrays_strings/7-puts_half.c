572dace6c8860eee928585c717a38470ac8f93c5
shakemboi <shakemboi12@gmail.com>
2023-10-17 10:52:24 -0700

    puts half

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -8,19 +8,26 @@
  */
 void puts_half(char *str)
 {
-	int a, n, longi;
+	int j = 0;
+	int k;
 
-	longi = 0;
+	while (str[j] != '\0')
+	{
+		j++;
+	}
+	if (j % 2 == 1)
+	{
+		k = (j - 1) / 2;
+		k += 1;
+	}
+	else
+	{
+		k = j / 2;
+	}
 
-	for (a = 0; str[a] != '\0'; a++)
-		longi++;
-
-	n = (longi /2);
-
-	if ((longi % 2) == 1)
-		n= ((longi + 1) / 2);
-
-	for (a = n ; str[a] != '\0'; a++)
-		_putchar(str[a]);
+	for (; k < j; k++)
+	{
+		_putchar(str[k]);
+	}
 	_putchar('\n');
 }
