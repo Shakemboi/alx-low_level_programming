c3d2878db6fc488996f3310a79baa13613bbf2c7
shakemboi <shakemboi12@gmail.com>
2023-10-23 12:18:18 -0700

    5-strstr

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+  * _strstr - a function that locates a substring
+  *
+  * @needle: function parameter one
+  *
+  * @haystack: function parameter two
+  *
+  * Return: Always 0.
+  */
+char *_strstr(char *haystack, char *needle)
+{
+	for (; *haystack != '\0'; haystack++)
+	{
+	char *I = haystack;
+	char *P = needle;
+
+	while (*I == *P && *P != '\0')
+	{
+	I++;
+	P++;
+	}
+	if (*P == '\0')
+	return (haystack);
+	}
+	return (0);
+}
