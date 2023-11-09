e80bb8c6ee605c514aafcb8f46eb9e8958d95fd4
shakemboi <shakemboi12@gmail.com>
2023-11-09 10:07:48 -0800

    print strings

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -16,6 +16,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	char *str;
 	unsigned int index;
 
+	va_start(strings, n);
 	for (index = 0; index < n; index++)
 	{
 		str = va_arg(strings, char *);
