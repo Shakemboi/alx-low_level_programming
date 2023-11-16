94c32d9375073617b7888b3a6e0bdd4d78f3e1ea
shakemboi <shakemboi12@gmail.com>
2023-11-16 14:45:56 -0800

    lists.h

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -5,21 +5,6 @@
 #include <string.h>
 #include <stddef.h>
 
-/**
- * struct list_t - its singly linked list
- * @str: sring - (for malloc 'ed string)
- * @len: its the string's length
- * @next: it points to the next node
- */
-typedef struct list_t
-{
-	char *str;
-
-	unsighned int len;
-
-	struct list_t *next;
-
-} list_t:
 size_t list_len(const list_t *h);
 size_t print_list(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
